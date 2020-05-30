6396256d5cb835fb15a8d492b00d9dbdeacafdc5
Aswani <achuaswani.g@gmail.com>
2020-05-30 00:31:22 -0700

    register test case

--- BaseAppUITests/RegisterScreen.swift
+++ BaseAppUITests/RegisterScreen.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class RegisterScreen: BaseAppUITests {
+    
     func test_registerFailureTest() {
         appLaunch()
         let loginScreen = LoginScreen()
@@ -18,6 +19,29 @@ class RegisterScreen: BaseAppUITests {
         let errorLabel = app.staticTexts["errorLabel"]
         XCTAssertEqual(errorLabel.label, "All fields are mandatory.")
         sleep(5)
+    }
+    
+    func test_registerNewAccount() {
+        let email = app.textFields["emailTextField"]
+        let identifier = UUID()
+        email.tap()
+        email.typeText(("test\(identifier)@gmail.com"))
+        app.keyboards.buttons["return"].tap()
+        let password = app.secureTextFields["passwordField"]
+        password.tap()
+        password.typeText("abcd1234")
+        app.keyboards.buttons["return"].tap()
+         let confirmPassword = app.secureTextFields["confirmPasswordField"]
+        confirmPassword.tap()
+        confirmPassword.typeText("abcd1234")
+        app.keyboards.buttons["return"].tap()
+        let displayName = app.textFields["userNameTextField"]
+        displayName.tap()
+        displayName.typeText("test\(identifier)")
+        let registerButton = app.buttons["registerButton"]
+        registerButton.tap()
+        let dashBoardView = app.otherElements["dashboard"]
+        XCTAssert(dashBoardView.waitForExistence(timeout: 5))
     }
     
 }
